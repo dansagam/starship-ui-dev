8cb9afb901ae67f4b1c72a92a901de1101133225
Kayode Ogunnowo <48393847+dansagam@users.noreply.github.com>
2024-02-09 09:03:20 +0100

    chore: add the link to home screen

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -1,4 +1,5 @@
 // import React from "react";
+import { Link } from "react-router-dom";
 import { MAIN_SIDEBAR, OVERVIEW_SIDEBAR, sidebar_logo } from "./sidebar-data";
 import SidebarLink from "./SidebarLink";
 
@@ -6,7 +7,9 @@ function Sidebar() {
   return (
     <aside className=" sticky top-0 h-full overflow-y-auto w-[320px] min-w-[520px]:w-[90px] md:w-[320px] bg-sidebar-main py-6 px-5 flex flex-col gap-7">
       <div>
-        <img loading="eager" src={sidebar_logo} alt="LOGO" />
+        <Link to="/login">
+          <img loading="eager" src={sidebar_logo} alt="LOGO" />
+        </Link>
       </div>
       <div className=" flex flex-col gap-3 mb-3">
         {OVERVIEW_SIDEBAR.map((field) => (
